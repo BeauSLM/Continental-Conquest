d40e4853b49c7ab39af7170b92b6bf906ce18832
Michael Corleone <32565641+MichaelDonCorleone@users.noreply.github.com>
2021-12-16 11:45:57 -0700

    initial Serializer

--- Continental_Conquest/api/serializers.py
+++ Continental_Conquest/api/serializers.py
@@ -21,7 +21,6 @@ class AdminSerializer(serializers.ModelSerializer):
             'admin',
             'perm_level'
         )
-
 class CharbagSerializer(serializers.ModelSerializer):
     class Meta:
         model = CharBag
@@ -29,6 +28,29 @@ class CharbagSerializer(serializers.ModelSerializer):
             'acc',
             'char_name',
             'item'
+        )
+
+class CharslotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CharSlots
+        fields = (
+            'acc',
+            'char_name',
+            'item',
+            'slot_type'
+        )
+
+class CharstatsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CharStats
+        fields = (
+            'acc',
+            'char_name',
+            'atk',
+            'def_field',
+            'hp',
+            'mp',
+            'spd'
         )
 
 class UserSerializer(serializers.ModelSerializer):
@@ -42,4 +64,138 @@ class UserSerializer(serializers.ModelSerializer):
             'password',
             'birthday',
             'email'
+        )
+
+
+
+class CharactersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Characters
+        fields = (
+            'acct',
+            'name',
+            'lvl',
+            'xp',
+            'gold',
+            'location',
+            'race',
+            'class_field',
+            'party'
+        )
+
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class 
+        fields = (
+            'name',
+            'description',
+        )
+
+class ClassabilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClassAbility
+        fields = (
+            'abil_name',
+            'class_field'
+        )
+
+class FriendListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FriendList
+        fields = (
+            'acct',
+            'friend'
+        )
+
+class GuildSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guild
+        fields = (
+            'guild_name',
+            'leader',
+            'xp',
+            'level',
+            'gold'
+        )
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = (
+            'item_id',
+            'name',
+            'type',
+            'sel_price',
+            'rarity',
+            'description',
+            'item_category',
+            'base_dmg',
+            'base_def'
+        )
+
+class ItemclassreqSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = ItemClassReq
+        fields = (
+            'item',
+            'class_field'
+        )
+
+class ItemstatsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemStats
+        fields = (
+            'item',
+            'atk',
+            'def_field',
+            'hp',
+            'mp',
+            'spd',
+        )
+
+class PartySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Party
+        fields = (
+            'party_id',
+            'acct',
+            'ch_name'
+        )
+class PlayersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Players
+        fields = (
+            'player',
+            'playtime',
+            'sub_status',
+            'guild'
+        )
+
+class RaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Race
+        fields = (
+            'name',
+            'description'
+        )
+
+class RaceabilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RaceAbility
+        fields = (
+            'abil_name',
+            'race'
+        )
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = (
+            'ticket_id',
+            'issue',
+            'category',
+            'date',
+            'player',
+            'admin',
+            'status'
         )
