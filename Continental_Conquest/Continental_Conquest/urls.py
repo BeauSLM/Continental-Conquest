40415af09f20f2917d1db8125e401ae264af21aa
Beau McCartney <mccartney.beausl@gmail.com>
2021-12-16 12:47:45 -0700

    Another api request test

--- Continental_Conquest/Continental_Conquest/urls.py
+++ Continental_Conquest/Continental_Conquest/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'players', views.PlayerViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'^', include('api.urls'))
+    path('', include(router.urls)),
+    path('api-auto/', include('rest_framework.urls', namespace='rest_framework'))
 ]
