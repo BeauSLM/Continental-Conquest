70857be92041224408dbe79903434bc5db16e2e3
Beau McCartney <mccartney.beausl@gmail.com>
2021-12-16 10:52:52 -0700

    Add api_endpoints specification

--- Continental_Conquest/api/models.py
+++ Continental_Conquest/api/models.py
@@ -4,13 +4,9 @@ from django.db import models
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from django.db import models
-
-
 class Ability(models.Model):
     name = models.CharField(db_column='Name', primary_key=True, max_length=30)  # Field name made lowercase.
     mana_cost = models.IntegerField(db_column='Mana_Cost')  # Field name made lowercase.
