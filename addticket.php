4db2f9b1a56bd7ad480addda12213d2a1bd49f90
Michael Corleone <32565641+MichaelDonCorleone@users.noreply.github.com>
2021-12-16 12:34:06 -0700

    initial create ticket

---
+++ addticket.php
@@ -0,0 +1,84 @@
+<?php
+    include 'include/connectDB.php';
+    if(!isset($_COOKIE["admin"]) || !isset($_COOKIE["password"])) {
+        header("Location: index.php");
+        exit("");
+    }
+    include 'include/header.php';
+    include 'include/left-menu.php';
+    //viewcharacter.php?char_name=AlexanderTheGreat&acct_id
+    
+    if(isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["category"]) && isset($_POST["issue"])) {
+        //if we have set all the post variables when calling remove item
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $category = $_POST["category"];
+        $issue = $_POST["issue"]
+
+        $stmt = $db->connection->prepare('SELECT * FROM USERS WHERE Username = ? AND Password = ?');
+        $stmt->bind_param('ss', $item_ID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if($result->num_rows == 0) {
+            echo "
+                <div class='center'>
+                    <div id='center-content'>
+                        Insert Failed: Item ID does not exist in database.
+                        <br/><a href='editcharacter.php?acct_id=".$acct_id."&char_name=".$char_name."'>Edit character: ".$char_name."</a>
+                        <br/><a href='selecteditcharacter.php'>Edit another character</a> 
+                        <br/><a href='index.php'>Return to Main Menu</a>
+                    </div>
+                </div>
+                ";
+        }
+        else {
+            $stmt = $db->connection->prepare('SELECT * FROM CHAR_BAG WHERE Acc_ID = ? AND Char_Name = ? AND Item_ID = ?');
+            $stmt->bind_param('isi', $acct_id, $char_name, $item_ID);
+            $stmt->execute();
+            $checkItem = $stmt->get_result();
+
+            if($checkItem->num_rows > 0) {
+                echo "
+                <div class='center'>
+                    <div id='center-content'>
+                        Insert Failed: User already has item in bag. Go back to main page. <a href='index.php'>Index</a>
+                        <br/><a href='editcharacter.php?acct_id=".$acct_id."&char_name=".$char_name."'>Edit character: ".$char_name."</a>
+                        <br/><a href='selecteditcharacter.php'>Edit another character</a> 
+                        <br/><a href='index.php'>Return to Main Menu</a>
+                    </div>
+                </div>
+                ";
+            } else {
+                $stmt = $db->connection->prepare('INSERT INTO CHAR_BAG(Acc_ID, Char_Name, Item_ID) VALUES(?, ?,  ?)');
+                $stmt->bind_param('isi', $acct_id, $char_name, $item_ID);
+                $stmt->execute();
+                echo "
+                <div class='center'>
+                    <div id='center-content'>
+                        Insert Success: Item with criteria : Account ID -> ".$acct_id."., Character Name-> ".$char_name.", Item ID -> ".$item_ID." was added to bag.
+                        <br/><a href='editcharacter.php?acct_id=".$acct_id."&char_name=".$char_name."'>Edit character: ".$char_name."</a>
+                        <br/><a href='selecteditcharacter.php'>Edit another character</a> 
+                        <br/><a href='index.php'>Return to Main Menu</a>
+                    </div>
+                </div> ";
+            }
+        }
+    } else {
+        //not correct fields
+        echo "
+            <div class='center'>
+                <div id='center-content'>
+                    Error: You have not specified the proper fields, this shouldn't happen.
+                    <br/><a href='editcharacter.php?acct_id=".$acct_id."&char_name=".$char_name."'>Edit character: ".$char_name."</a>
+                    <br/><a href='selecteditcharacter.php'>Edit another character</a> 
+                    <br/><a href='index.php'>Return to Main Menu</a>
+                </div>
+            </div>
+            ";
+    }
+?>
+<?php
+    include 'include/right-menu.php';
+    include 'include/footer.php';
+?>
--- createticket.php
+++ createticket.php
@@ -12,198 +12,49 @@
 <div id='center-content'>
     <table id='table_players'>
         <tr class='table_players_row'>
-            <td class='table_players_data' id='table_players_title' colspan='3' >
+            <td class='table_players_data' id='table_players_title' colspan='2' >
                 Create Ticket : 
-                    <?php 
-                        if ($result->num_rows == 0) { 
-                            echo "<br/>Character not found.</td></tr>";
-                        } else {
-                            $row = $result->fetch_row();
-                            echo $row[1];
-        echo "
-                </td>
-            </tr>
-            <form action='updatecharacter.php' method='post'>
-                <input type='hidden' name='acct_id' value='".$acct_id."'/>
-                <input type='hidden' name='char_name' value='".$char_name."'/>
+            </td>
+        </tr>
+            <form action='create_ticket.php' method='post'>
                 <tr class='table_players_row'>
                     <td class='table_players_data'>
-                        Player ID :
+                        Player Usermame :
                     </td>
                     <td class='table_players_data'>
-                        ".$row[0]."
+                        <input class="input-fields" onfocus="this.value=''" type="text" name="username" value="Type Username" />
                     </td>
                 </tr>
                 <tr class='table_players_row'>
                     <td class='table_players_data'>
-                        Character Name :
+                        Player Password :
                     </td>
                     <td class='table_players_data'>
-                        ".$row[1]."
-                    </td>
-                </tr>
-                <tr class='table_players_row'>
-                    <td class='table_players_data'>
-                        Level :
-                    </td>
-                    <td class='table_players_data'>
-                        <input name='level' class='input-fields' type='text' value='".$row[2]."' />
+                        <input class="input-fields" onfocus="this.value=''" type="password" name="password" value="Type password" />
                     </td>
                 </tr>
                 <tr class='table_players_row'>
                     <td class='table_players_data'>
-                        Experience :
+                        Issue
                     </td>
                     <td class='table_players_data'>
-                        <input name='xp' class='input-fields' type='text' value='".$row[3]."' />
-                    </td>
-                </tr>
-                <tr class='table_players_row'>
-                    <td class='table_players_data'>
-                        Gold :
-                    </td>
-                    <td class='table_players_data'>
-                        <input name='gold' class='input-fields' type='text' value='".$row[4]."' />
+                        <textarea class="input-fields" onfocus="this.value=''" name="w3review" rows="4" cols="50" value>Type your issue here</textarea>
                     </td>
                 </tr>
                 <tr class='table_players_row'>
                     <td class='table_players_data'>
-                        Location :
+                        Category :
                     </td>
                     <td class='table_players_data'>
-                        <input name='location' class='input-fields' type='text' value='".$row[5]."' />
-                    </td>
-                </tr>
-                <tr class='table_players_row'>
-                    <td class='table_players_data'>
-                        Race :
-                    </td>
-                    <td class='table_players_data'>
-                        <input name='race' class='input-fields' type='text' value='".$row[6]."' />
-                    </td>
-                </tr>
-                <tr class='table_players_row'>
-                    <td class='table_players_data'>
-                        Class :
-                    </td>
-                    <td class='table_players_data'>
-                        <input name='class' class='input-fields' type='text' value='".$row[7]."' />
+                        <input  class='input-fields' onfocus="this.value=''" name='category' type='text' value='Type Category' />
                     </td>
                 </tr>
                 <tr class='table_players_row'>
                     <td class='table_players_data' colspan='2'>
-                        <input type ='submit' class='submit-inputs' value='Update Character' />
-                    </td>
-                </tr>
-                <tr class='table_players_row'>
-                    <td class='table_players_data' colspan='2'>
-                        Note: Changing race or class can result in equipped items with requirements being unequipped when the player enters the game.
+                        <input type ='submit' class='submit-inputs' value='Create Ticket' />
                     </td>
                 </tr>
             </form>
-            ";
-            ?>
-            <br/>
-            <br/>
-            <table id="table_players">
-                <tr class="table_players_row">
-                    <td class="table_players_data" id="table_players_title" colspan="7" >
-                        Remove Item
-                    </td>
-                </td>
-
-                <?php
-                    //fetch all equipped items
-                    $stmt = $db->connection->prepare('SELECT * FROM ITEM NATURAL JOIN CHAR_SLOTS WHERE Acc_ID = ? AND Char_name = ? ORDER BY Item_ID');
-                    $stmt->bind_param('is', $acct_id, $char_name);
-
-                    $stmt->execute();
-                    $equipped = $stmt->get_result();
-
-                    $stmt = $db->connection->prepare('SELECT * FROM ITEM NATURAL JOIN CHAR_BAG WHERE Acc_ID = ? AND Char_name = ? ORDER BY Item_ID');
-                    $stmt->bind_param('is', $acct_id, $char_name);
-
-                    $stmt->execute();
-                    $bag = $stmt->get_result();
-                echo "
-                <tr class='table_players_row'>
-                    <form action='removeitem.php' method='post'>
-                    <td class='table_players_data'>
-                        Equipped: ";
-                        if($equipped->num_rows != 0) {
-                            echo"
-                            <input type='hidden' name='acct_id' value='".$acct_id."'/>
-                            <input type='hidden' name='char_name' value='".$char_name."'/>
-                            <input type='hidden' name='operation' value='slot' />
-                            <select name = 'item_id' class='input-fields' id='view-slot-items-selector'>
-                            <option value='select'>Select Item...</option>
-                            ";
-                            foreach($equipped as $item) {
-                                echo "<option value='".$item['Item_ID']."'>".$item['Item_ID']." : ".$item['Name']."</option>";
-                            }
-                            echo "</select>";
-                        } else { //if char isn't in party, tells them that
-                            echo "No Items";
-                        }
-                        echo"
-                    </td>
-                    <td class='table_players_data' colspan='2'>
-                        <input type ='submit' class='submit-inputs' value='Remove Item' />
-                    </td>
-                    </form>
-                </tr>
-                <tr class='table_players_row'>
-                    <form action='removeitem.php' method='post'>
-                    <td class='table_players_data'>
-                        Bag: ";
-                        if($bag->num_rows != 0) {
-                            echo"
-                            <input type='hidden' name='acct_id' value='".$acct_id."'/>
-                            <input type='hidden' name='char_name' value='".$char_name."'/>
-                            <input type='hidden' name='operation' value='bag' />
-                            <select name='item_id' class='input-fields' id='view-bag-items-selector'>
-                            <option value='select'>Select Item...</option>
-                            ";
-                            foreach($bag as $item) {
-                                echo "<option value='".$item['Item_ID']."'>".$item['Item_ID']." : ".$item['Name']."</option>";
-                            }
-                            echo "</select>";
-                        } else { //if char isn't in party, tells them that
-                            echo "No Items";
-                        }
-                        echo"
-                    </td>
-                    <td class='table_players_data' colspan='2'>
-                        <input type ='submit' class='submit-inputs' value='Remove' />
-                    </td>
-                    </form>
-                </tr>
-                ";
-                ?>
-            </table>
-            
-            <table id="table_players">
-                <tr class="table_players_row">
-                    <td class="table_players_data" id="table_players_title" colspan="2" >
-                        Add Item to Bag
-                    </td>
-                </tr>
-                <tr class='table_players_row'>
-                <form action='additem.php' method='post'>
-                    <td class='table_players_data'>
-                        <input type="hidden" name="acct_id" value="<?php echo $acct_id; ?>" />
-                        <input type="hidden" name="char_name" value="<?php echo $char_name; ?>" />
-                        <input class="input-fields" onfocus="this.value=''" name="item_id" type="text" value="Type Item ID..." />
-                    </td>   
-                    <td class='table_players_data' colspan='2'>
-                        <input type ='submit' class='submit-inputs' value='Add Item' />
-                    </td>
-                </form>   
-                </tr>
-
-            <?php
-            }
-            ?>
         </table>
     </div>
 </div>
