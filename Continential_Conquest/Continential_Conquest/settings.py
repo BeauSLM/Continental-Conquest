3946dc27fe833918e6be057ffa1a911c118b12aa
Beau McCartney <mccartney.beausl@gmail.com>
2021-12-16 01:40:54 -0700

    Connect db to api

--- Continential_Conquest/Continential_Conquest/settings.py
+++ Continential_Conquest/Continential_Conquest/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Continential_Conquest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CONTINENTAL_CONQUEST',
+        'USER': 'ensf409',
+        'PASSWORD': 'ensf409',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
