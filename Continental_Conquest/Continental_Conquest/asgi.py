5d524967841a264bbeb08f7c424fa2383008b2ff
Beau McCartney <mccartney.beausl@gmail.com>
2021-12-16 03:43:22 -0700

    Fix spelling error holy fuck

--- Continental_Conquest/Continental_Conquest/asgi.py
+++ Continental_Conquest/Continental_Conquest/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Continential_Conquest project.
+ASGI config for Continental_Conquest project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Continential_Conquest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Continental_Conquest.settings')
 
 application = get_asgi_application()
