1e716884c21e84b16f06de7b9d6c67073b9766f0
Beau McCartney <mccartney.beausl@gmail.com>
2021-12-23 16:45:01 -0700

    Rename REST-api project

--- REST-api/REST-api/urls.py
+++ REST-api/REST-api/urls.py
@@ -1,4 +1,4 @@
-"""Continental_Conquest URL Configuration
+"""REST-api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
